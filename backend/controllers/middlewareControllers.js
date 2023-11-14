af29bc22b8356890cb4c2437a8991876da45effa
Dang Gia Loc <74612458+locdang1999@users.noreply.github.com>
2023-11-15 01:11:13 +0700

    done refresh token FE

--- backend/controllers/middlewareControllers.js
+++ backend/controllers/middlewareControllers.js
@@ -9,24 +9,24 @@ const middlewareController = {
       const accessToken = token.split(" ")[1];
       jwt.verify(accessToken, process.env.JWT_ACCESS_KEY, (err, user) => {
         if (err) {
-          res.status(403).json("Token is not valid");
+          return res.status(403).json("Token is not valid");
         }
         req.user = user;
         next();
       });
     } else {
-      res.status(401).json("You're not authentication");
+      return res.status(401).json("You're not authentication");
     }
   },
 
   verifyTokenAndAdminAuth: (req, res, next) => {
-    middlewareController.verifyToken(req, res, ()=>{
-        if(req.user.id == req.params.id || req.user.admin){
-            next();
-        }else{
-            res.status(401).json("You're not allowed to delete other");
-        }
-    })
+    middlewareController.verifyToken(req, res, () => {
+      if (req.user.id == req.params.id || req.user.admin) {
+        next();
+      } else {
+        return res.status(401).json("You're not allowed to delete other");
+      }
+    });
   },
 };
 
