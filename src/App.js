e728fce529af40137fac1277e509bce02ed4ca77
Dang Gia Loc <74612458+locdang1999@users.noreply.github.com>
2023-11-16 18:38:41 +0700

    done login UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Login from './Login';
 import Register from './Register';
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      {/* <Register /> */}
+      <Login />
     </div>
   );
 }
--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,103 @@
-import React from 'react'
+import React, { useContext, useEffect, useRef, useState } from "react";
+import AuthContext from "./context/AuthProvider";
+import axios from "./api/axios";
+
+const LOGIN_URL = "/auth";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const { setAuth } = useContext(AuthContext);
+  const userRef = useRef();
+  const errRef = useRef();
 
-export default Login
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const resp = await axios.post(LOGIN_URL, JSON.stringify({ user, pwd }), {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true,
+      });
+
+      const accessToken = resp?.data?.accessToken;
+      const roles = resp?.data?.roles;
+      setAuth({ user, pwd, roles, accessToken });
+      setUser("");
+      setPwd("");
+      setSuccess(true);
+    } catch (error) {
+      if (!error.response) {
+        setErrMsg("No Server Response");
+      } else if (error.response?.status === 400) {
+        setErrMsg("Missing Username or Password.");
+      }else if (error.response?.status === 401) {
+        setErrMsg("Unauthorizaed.");
+      } else {
+        setErrMsg("Login Failed.");
+      }
+      errRef.current.focus();
+    }
+  };
+
+  return (
+    <>
+      {success ? (
+        <section>
+          <h1>You are logged in!</h1>
+          <br />
+          <p>
+            <a href="#">Go to Home</a>
+          </p>
+        </section>
+      ) : (
+        <section>
+          <p
+            ref={errRef}
+            className={errMsg ? "errmsg" : "offscreen"}
+            aria-live="assertive"
+          >
+            {errMsg}
+          </p>
+          <h1>Sign In</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              ref={userRef}
+              autoComplete="off"
+              onChange={(e) => setUser(e.target.value)}
+              value={user}
+              required
+            />
+
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              onChange={(e) => setPwd(e.target.value)}
+              value={pwd}
+              required
+            />
+            <button>Sign In</button>
+          </form>
+        </section>
+      )}
+    </>
+  );
+};
+
+export default Login;
